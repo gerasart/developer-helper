3f7c3b97be04bf3ebc8b151d0caac558d714bf27
Gerasart <gerasymenkoph@gmail.com>
2019-01-21 16:19:30 +0200

    clear (auto push)

--- developer-helper.php
+++ developer-helper.php
@@ -71,7 +71,7 @@ class DeveloperHelper {
 new DeveloperHelper();
 
 
-require 'plugin-update-checker/plugin-update-checker.php';
+require './inc/plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/gerasart/developer-helper/',
 	__FILE__,
